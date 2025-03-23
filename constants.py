84ae7d9e35426e99d8fd8f78efd876f30fc4a8aa
Preet Karia <karia.preet5@gmail.com>
2025-03-23 17:07:55 -0400

    Initialize

---
+++ constants.py
@@ -0,0 +1,40 @@
+PK_TRAIN_1_2 = 'data_test_3/PK_TRAIN_1_2.xlsx'
+PK_TRAIN_2 = 'data_test_1/PK_TRAIN_2.xlsx'
+PK_TEST_1 = 'data_test_1/PK_TEST_1.xlsx'
+PK_TRAIN_1 = 'data_test_2/PK_TRAIN_1.xlsx'
+PK_TEST_2 = 'data_test_2/PK_TEST_2.xlsx'
+PK_TEST_3 = 'data_test_3/PK_TEST_3.xlsx'
+
+SA_TRAIN_1_2 = 'data_test_3/SA_TRAIN_1_2.xlsx'
+SA_TRAIN_2 = 'data_test_1/SA_TRAIN_2.xlsx'
+SA_TEST_1 = 'data_test_1/SA_TEST_1.xlsx'
+SA_TRAIN_1 = 'data_test_2/SA_TRAIN_1.xlsx'
+SA_TEST_2 = 'data_test_2/SA_TEST_2.xlsx'
+SA_TEST_3 = 'data_test_3/SA_TEST_3.xlsx'
+
+NG_TRAIN_1_2 = 'data_test_3/NG_TRAIN_1_2.xlsx'
+NG_TRAIN_2 = 'data_test_1/NG_TRAIN_2.xlsx'
+NG_TEST_1 = 'data_test_1/NG_TEST_1.xlsx'
+NG_TRAIN_1 = 'data_test_2/NG_TRAIN_1.xlsx'
+NG_TEST_2 = 'data_test_2/NG_TEST_2.xlsx'
+NG_TEST_3 = 'data_test_3/NG_TEST_3.xlsx'
+
+
+NU_TRAIN_1_2 = 'data_test_3/NU_TRAIN_1_2.xlsx'
+NU_TRAIN_2 = 'data_test_1/NU_TRAIN_2.xlsx'
+NU_TEST_1 = 'data_test_1/NU_TEST_1.xlsx'
+NU_TRAIN_1 = 'data_test_2/NU_TRAIN_1.xlsx'
+NU_TEST_2 = 'data_test_2/NU_TEST_2.xlsx'
+NU_TEST_3 = 'data_test_3/NU_TEST_3.xlsx'
+
+
+BY_TRAIN_1_2 = 'data_test_3/BY_TRAIN_1_2.xlsx'
+BY_TRAIN_2 = 'data_test_1/BY_TRAIN_2.xlsx'
+BY_TEST_1 = 'data_test_1/BY_TEST_1.xlsx'
+BY_TRAIN_1 = 'data_test_2/BY_TRAIN_1.xlsx'
+BY_TEST_2 = 'data_test_2/BY_TEST_2.xlsx'
+BY_TEST_3 = 'data_test_3/BY_TEST_3.xlsx'
+
+
+ESD_TRAINING = 'esd_data/training.xlsx'
+ESD_TESTING = 'esd_data/testing.xlsx'
